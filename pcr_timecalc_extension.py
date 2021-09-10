3458a4586efbc3a0de1ef62ba62eb8c6c1fd04c9
Akitsuki <18420831+qiuyue0@users.noreply.github.com>
2021-09-10 22:15:42 +0800

    修复了bug

--- pcr_timecalc_extension.py
+++ pcr_timecalc_extension.py
@@ -20,8 +20,8 @@ async def feedback(bot, ev: CQEvent):
     dmg1=d1
     t=float(time)-1+0.01
     reply=f"刀1伤害：{d1}    Boss血量：{rest}    所需补时：{time}秒\n"
-    dmg2 = round(max(0,1/(109.01-t)*90*(rest-dmg1)),1)
+    dmg2 = round(max(0,1/(110.0-t)*90*(rest-dmg1)),2)
     reply+="第一刀先出，第二刀需要造成"+str(dmg2)+"伤害才能使第二刀获得"+str(time)+"秒补时\n"
-    dmg2 = round(max(0,rest-(109.01-t)/90*dmg1),1)
+    dmg2 = round(max(0,rest-(110.0-t)/90*dmg1),2)
     reply+="第一刀后出，第二刀需要造成"+str(dmg2)+"伤害才能使第一刀获得"+str(time)+"秒补时"
     await bot.send(ev, reply)
